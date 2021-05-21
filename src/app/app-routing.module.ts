4db20d03b7cfe248d0899285ef3f572e066165dd
bobbysaprey420 <bobbysaprey420@gmail.com>
2021-05-21 16:39:52 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { StateComponent } from './state/state.component';
+import { HomeComponent } from './home/home.component';
+import { DistrictComponent } from './district/district.component';
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  { path:'state/:id',component: StateComponent},
+  {path:'state/district/:id',component:DistrictComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { StateComponent } from './state/state.component';
+import { HomeComponent } from './home/home.component';
+import { DistrictComponent } from './district/district.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StateComponent,
+    HomeComponent,
+    DistrictComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
